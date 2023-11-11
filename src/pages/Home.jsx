73a42169e617936a2d8950ef5f7a9aa26d9e4176
rezaazimibar <www.beta04@gmail.com>
2023-11-11 20:30:10 +0330

    init main page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnapshot } from "valtio";
+import state from "../store";
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
 
 const Home = () => {
+  const snap = useSnapshot(state);
   return (
-    <div>Home</div>
-  )
-}
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation("left")}>
+          <motion.header {...slideAnimation("down")}>
+            <img
+              src="./threejs.png"
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+          <motion.div {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                let's
+                <br className="xl:block hidden" /> do it
+              </h1>
+            </motion.div>
+            <motion.div {...headContentAnimation}>
+              <p className="max-w-md font-normal text-gray-600 text-base">
+                Create your unique and exclusive shirt with our brand-new 3D
+                customization tool.<strong>Unleash your imagination</strong> and
+                define your own style.
+              </p>
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  );
+};
 
-export default Home
+export default Home;
