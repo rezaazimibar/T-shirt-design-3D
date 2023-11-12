0ae6a15b76609caf7cf03460f08cacbf4a9782fd
rezaazimibar <www.beta04@gmail.com>
2023-11-12 23:19:04 +0330

    home page

---
+++ src/components/CustomButton.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useSnapshot } from "valtio";
+import state from "../store";
+const CustomButton = ({ title, customStyles, type, handleClick }) => {
+  const snap = useSnapshot(state);
+  const generateStyle = (type) => {
+    if (type === "filled") {
+      return {
+        backgroundColor: snap.color,
+        color: "#fff",
+      };
+    }
+  };
+  console.log(snap);
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generateStyle(type)}
+      onClick={handleClick}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default CustomButton;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 import state from "../store";
+import { CustomButton } from "../components";
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -34,6 +35,12 @@ const Home = () => {
                 customization tool.<strong>Unleash your imagination</strong> and
                 define your own style.
               </p>
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
             </motion.div>
           </motion.div>
         </motion.section>
